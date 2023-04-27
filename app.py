c3536c12f48e547a9197edcaff21077017991616
Lucas Carvalho-- <carvalholcss@gmail.com>
2023-04-27 20:57:36 -0300

    Corrigido erro de CORS

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
+
 
 
 class Multiply(Resource):
