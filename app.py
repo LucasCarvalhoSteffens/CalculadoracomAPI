b29c8faf1b43f681c86fd75d1815d42da92688fb
Lucas Carvalho-- <carvalholcss@gmail.com>
2023-04-27 19:53:47 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Multiply(Resource):
+    def get(self):
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+        result = num1 * num2
+        return jsonify({'result': result})
+
+class Divide(Resource):
+    def get(self):
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+        result = num1 / num2
+        return jsonify({'result': result})
+
+class Add(Resource):
+    def get(self):
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+        result = num1 + num2
+        return jsonify({'result': result})
+
+class Subtract(Resource):
+    def get(self):
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+        result = num1 - num2
+        return jsonify({'result': result})
+
+
+api.add_resource(Multiply, '/multiply')
+api.add_resource(Divide, '/divide')
+api.add_resource(Add, '/add')
+api.add_resource(Subtract, '/subtract')
+
+if __name__ == '__main__':
+    app.run()
